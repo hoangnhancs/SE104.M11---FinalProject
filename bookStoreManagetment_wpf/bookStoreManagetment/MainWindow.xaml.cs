7c2b7d2932f820e6e11631c6e8c50755e02b9dbe
hoangthang019 <18521394@gm.uit.edu.vn>
2021-10-24 20:39:29 +0700

    fix effect click button

--- bookStoreManagetment_wpf/bookStoreManagetment/MainWindow.xaml.cs
+++ bookStoreManagetment_wpf/bookStoreManagetment/MainWindow.xaml.cs
@@ -20,109 +20,109 @@ namespace bookStoreManagetment
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Button btnIsClick = new Button();
+        private Button btnIsClicked = new Button();
         private StackPanel spnIsVisiable = new StackPanel();
         public MainWindow()
         {
             InitializeComponent();
-            btnIsClick = null;
+            btnIsClicked = null;
             spnIsVisiable = null;
         }
-
-        // đổi màu nút đã bấm 
-        private void ChangeColorButtonClicked(Button btn,bool isDuplicate = false)
+        private void clickButtonEffect(Button btn, StackPanel stkPanel = null)
         {
-            if (btnIsClick != null)
+            // tắt màu
+            if (btnIsClicked != null)
             {
-                btnIsClick.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
-            }
-            if (isDuplicate==false)
-            {
-                btn.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-            }
-            btnIsClick = btn;
-        }
-
-        // hàm show/hidden sub menu
-        private void hiddenSubMenu(StackPanel stkPanel, Button btn)
-        {
-            bool isDuplicate = false;
-            if (stkPanel.Visibility != Visibility.Visible)
-            {
-                stkPanel.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                isDuplicate = true;
-                stkPanel.Visibility = Visibility.Collapsed;
+                btnIsClicked.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
             }
 
-            if(spnIsVisiable != null && spnIsVisiable != stkPanel)
+            // tắt panel đang mở 
+            if (spnIsVisiable != null)
             {
                 spnIsVisiable.Visibility = Visibility.Collapsed;
             }
-            ChangeColorButtonClicked(btn, isDuplicate);
-            spnIsVisiable = stkPanel;
+
+            btn.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            btnIsClicked = btn;
+
+            if (stkPanel != null)
+            {
+                if (stkPanel != spnIsVisiable)
+                {
+                    stkPanel.Visibility = Visibility.Visible;
+                    spnIsVisiable = stkPanel;
+                }
+                else
+                {
+                    btnIsClicked.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+                    btnIsClicked = null;
+                    spnIsVisiable = null;
+                }
+            }
+            else
+            {
+                spnIsVisiable = null;
+            }
         }
 
         private void btnQLVatPham_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            hiddenSubMenu(stackPanelSubMenuQLVP,btn);
+            clickButtonEffect(btn, stackPanelSubMenuQLVP);
         }
 
         private void btnBanVatPham_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            hiddenSubMenu(stackPanelSubMenuBanVP,btn);
+            clickButtonEffect(btn, stackPanelSubMenuBanVP);
         }
 
         private void btnQLNhanVien_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            hiddenSubMenu(stackPanelSubMenuQLNV,btn);
+            clickButtonEffect(btn, stackPanelSubMenuQLNV);
         }
 
         private void btnQLKhachHang_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            hiddenSubMenu(stackPanelSubMenuQLKH,btn);
+            clickButtonEffect(btn, stackPanelSubMenuQLKH);
         }
 
         private void btnQLThuChi_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            hiddenSubMenu(stackPanelSubMenuQLThuChi,btn);
+            clickButtonEffect(btn, stackPanelSubMenuQLThuChi);
         }
 
         private void btnDashboard_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            ChangeColorButtonClicked(btn);
+            clickButtonEffect(btn);
         }
 
         private void btnQLBaoCao_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            ChangeColorButtonClicked(btn);
+            clickButtonEffect(btn);
         }
 
         private void btnQLSuKien_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            ChangeColorButtonClicked(btn);
+            clickButtonEffect(btn);
         }
 
         private void btnQLMail_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            ChangeColorButtonClicked(btn);
+            clickButtonEffect(btn);
         }
 
         private void btnCaiDatChung_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            ChangeColorButtonClicked(btn);
+            clickButtonEffect(btn);
         }
     }
 }
